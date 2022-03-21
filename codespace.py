a1d39700a3e1f69203e0cd3965c0352ecee370a2
Hirdesh <hirdeshgarg0012@gmail.com>
2022-03-22 00:33:39 +0530

    last

--- codespace.py
+++ codespace.py
@@ -7,12 +7,16 @@ source = requests.get("http://www.mnit.ac.in/news/newsall.php?type=latest").text
 soup = BeautifulSoup(source, 'lxml')
 
 li = soup.find('div', {'class': 'tabcontent','id':'latestD'})
-children = li.findChildren('a')
+children = li.findChildren('a', href=True)
 temp_list = []
+temp_list2 = []
 for child in children:
 	temp_list.append(child.text)
+for child in children:
+	temp_list2.append('http://www.mnit.ac.in/news/'+child['href'])
 data = {
 	"news": temp_list,
+	"newsLink": temp_list2
 }
 jsson = json.dumps(data)
 print(jsson)
